f2052dd20e2fb35af3316ecf6eb927b7f2359f40
Wilson Gramer <wgramer03@gmail.com>
2020-07-26 13:10:22 -0400

    Fix python dependencies

--- wipple-highlighting/index.js
+++ wipple-highlighting/index.js
@@ -1,7 +1,7 @@
 const { execSync } = require('child_process')
 
 const highlightWipple = (code) => {
-    const output = execSync(`/usr/local/opt/python@3.8/bin/python3 ${__dirname}`, { input: code })
+    const output = execSync(`python3 ${__dirname}`, { input: code })
     return output.toString('utf8')
 }
 
