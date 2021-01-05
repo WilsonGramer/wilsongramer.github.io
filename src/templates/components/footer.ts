9bdf1e779f5aee6a7fd26be881834bca6e42898f
Wilson Gramer <wilson@gramer.dev>
2021-01-05 15:48:00 -0500

    Use current year

--- src/templates/components/footer.ts
+++ src/templates/components/footer.ts
@@ -2,7 +2,7 @@ import { htmlFragmentSync as html } from "lit-ntml";
 
 export const footer = () => html`
     <div class="p-6 text-sm text-center text-gray-500">
-        <p>© 2020 Wilson Gramer</p>
+        <p>© ${new Date().getFullYear()} Wilson Gramer</p>
         <p class="underline">
             <a href="https://github.com/WilsonGramer/website">Source code</a>
         </p>
