fd5ac28345f820409bea98757e8f67099c9e0c41
Wilson Gramer <wilson@gramer.dev>
2021-01-11 16:01:07 -0500

    Update styling

--- src/templates/styles/blog-post.ts
+++ src/templates/styles/blog-post.ts
@@ -1,4 +1,23 @@
 export const blogPostContentStylesheet = `
+    .markdown-content h1 {
+        font-size: 1.75rem;
+        margin: 1rem 0;
+    }
+
+    .markdown-content h2 {
+        font-size: 1.5rem;
+        margin: 1rem 0;
+    }
+
+    .markdown-content h3 {
+        font-size: 1.25rem;
+        margin: 1rem 0;
+    }
+
+    .markdown-content ol, .markdown-content ul {
+        list-style: initial;
+    }
+
     .markdown-content p {
         margin-bottom: 1em;
     }
@@ -15,5 +34,9 @@ export const blogPostContentStylesheet = `
         margin: 1em 0;
         padding: 1em;
         border-radius: 8pt;
+    }
+
+    .markdown-content hr {
+        margin: 2rem 0;
     }
 `;
