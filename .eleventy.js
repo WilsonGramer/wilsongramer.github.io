a973b41cc2c231304203e3a8807dd354959bd1f6
Wilson Gramer <12683684+WilsonGramer@users.noreply.github.com>
2022-08-05 15:50:09 -0400

    Add profile photo favicon

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,11 @@ const { format } = require("date-fns");
 module.exports = (eleventy) => {
     eleventy.setTemplateFormats(["html", "md", "jpg", "png", "svg"]);
 
+    eleventy.addGlobalData(
+        "profile",
+        "https://0.gravatar.com/avatar/1a55ce5c75a3f6759a6ecd47e99f7aac?s=200"
+    );
+
     eleventy.addShortcode("year", () => format(new Date(), "yyyy"));
     eleventy.addFilter("postDate", (date) => format(date, "MMMM d, yyyy"));
 
